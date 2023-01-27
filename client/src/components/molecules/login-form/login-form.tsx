2b2f4c64dfffd9c414f9809815c8e262e936e804
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2023-01-26 22:51:26 -0300

    feat: create hide on password

--- client/src/components/molecules/login-form/login-form.tsx
+++ client/src/components/molecules/login-form/login-form.tsx
@@ -1,6 +1,12 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { StyledForm, StyledLoginForm } from "./styles";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+
 export function LoginForm() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  function handleShowPassword() {
+    setShowPassword(!showPassword);
+  }
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const loginPayload = {
@@ -15,7 +21,20 @@ export function LoginForm() {
       <h2>Login</h2>
       <StyledForm onSubmit={handleSubmit}>
         <input placeholder="Your e-mail" name="email" />
-        <input placeholder="Your Password" name="password" />
+        <div>
+          <input
+            placeholder="Your Password"
+            type={showPassword ? "text" : "password"}
+            name="password"
+          />
+          <button onClick={handleShowPassword}>
+            {showPassword ? (
+              <BsEyeSlashFill size={20} />
+            ) : (
+              <BsEyeFill size={20} />
+            )}
+          </button>
+        </div>
         <button type="submit">Login</button>
       </StyledForm>
     </StyledLoginForm>
--- client/src/components/molecules/login-form/styles.ts
+++ client/src/components/molecules/login-form/styles.ts
@@ -28,8 +28,49 @@ export const StyledForm = styled.form`
     margin: 1rem;
     border-radius: 1rem;
     border: none;
+    height: 35px;
+    font-size: 1rem;
+    background-color: ${(props) => props.theme.light.fg};
+  }
+  div {
+    display: flex;
+    flex-direction: row;
+    width: 90%;
+    border-radius: 5px 0 0 5px;
+    border: none;
+    align-items: center;
+
+    input {
+      display: flex;
+      width: 100%;
+      padding: 0.5rem 0 0.5rem 1rem;
+      margin: 1rem -0.5rem 1rem 1rem;
+      border-radius: 1rem;
+      border: none;
+      height: 35px;
+    }
+    button {
+      height: 50%;
+      display: flex;
+      align-items: center;
+      border: none;
+      border-radius: 1rem;
+      margin: 0 1rem 0 -1rem;
+      padding: 0 0 0 -1rem;
+      cursor: pointer;
+      background-color: ${(props) => props.theme.light.fg};
+    }
   }
   button {
     margin: 1rem;
+    height: 90%;
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    border: none;
+    border-radius: 0.9rem;
+    background-color: ${(props) => props.theme.light.fg};
+    color: rgb(248,248,242);
+    cursor: pointer;
   }
 `;
