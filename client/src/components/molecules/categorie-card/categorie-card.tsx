0d55692246d98d3c918b23019251732c91f7f097
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2023-02-15 22:54:43 -0300

    feat: updated styles on home

--- client/src/components/molecules/categorie-card/categorie-card.tsx
+++ client/src/components/molecules/categorie-card/categorie-card.tsx
@@ -2,6 +2,8 @@ import { api } from "../../../utils/api/api";
 import { UpdateCategorieForm } from "../../celules/update-categorie-form/update-categorie-form";
 
 import { Categorie } from "../../types/data";
+import ProductList from "../products-list/products-list";
+import { CardInfoContainer, CategorieCardContainer, CategorieCardOptionsContainer } from "./styles";
 
 export type CategorieCardProps = {
   categorie: Categorie;
@@ -21,25 +23,29 @@ export function CategorieCard({
   }
 
   return (
-    <div>
-      <h2>{categorie?.name}</h2>
-      <button
-        onClick={() => {
-          changeEditingMode();
-        }}
-      >
-        Edit this categorie
-      </button>
-      <button onClick={handleDeleteCategorie}>Delete this categorie</button>
-      {editingMode ? (
-        <UpdateCategorieForm
-          handleControl={handleControl}
-          categorie={categorie}
-          changeEditingMode={changeEditingMode}
-        />
-      ) : (
-        <CreateCategorieForm handleControl={handleControl} />
-      )}
-    </div>
-  );
-}
+    <CategorieCardContainer>
+      <CardInfoContainer>
+        <section>
+          <span>Categorie:</span>
+          <h2>{categorie?.name}</h2>
+          </section>
+          </CardInfoContainer>
+          <CategorieCardOptionsContainer>
+          <button
+            onClick={() => {
+              changeEditingMode();
+            }}
+          >
+            Edit this categorie
+          </button>
+          <button onClick={handleDeleteCategorie}>Delete this categorie</button>
+          {editingMode ? (
+            <UpdateCategorieForm
+              handleControl={handleControl}
+              categorie={categorie}
+              changeEditingMode={changeEditingMode}
+            />
+          ): <ProductList selectedCategorie={undefined}/>}
+        </CategorieCardOptionsContainer>
+        </CategorieCardContainer>
+  )}
---
+++ client/src/components/molecules/categorie-card/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CategorieCardContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const CardInfoContainer = styled.div`
+  display: flex;
+  width: 80%;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const CategorieCardOptionsContainer = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
